3d8fcfadbd0f60c0339e05ecc65d1a8898534d51
AndreasSterner <andreas@sterner.io>
2017-09-10 19:23:18 +0200

    bleh

--- 5-fadingContent.js
+++ 5-fadingContent.js
@@ -1,1 +1,18 @@
 console.log("test");
+
+$("div").click(function(event) {
+  /* Act on the event */
+  $(this).fadeOut('slow', function() {
+
+  });
+});
+
+// Dont judge the terrible css
+$(".button").click(function(event) {
+  /* Act on the event */
+  $("#fadeThis").fadeOut('slow', function() {
+  $("#hidden").fadeIn('slow', function() {
+    $("#hidden").html("Such terrible fade")
+    });
+  });
+});
