3c9452a5a7776a757881e5124f886cfacfc18a9b
AndreasSterner <andreas@sterner.io>
2017-09-10 17:25:19 +0200

    jQuery ^-^

--- 1-jQueryBasics.js
+++ 1-jQueryBasics.js
@@ -1,1 +1,7 @@
 console.log("test"); //works
+
+if (typeof jQuery == "undefined") {
+  alert("jQuery is not installed")
+} else {
+  alert("jQuery is installed")
+}
---
+++ 2-detectingAClickjQuery.js
@@ -0,0 +1,7 @@
+ $("#circle").click(function(event) {
+   alert("Circle was clicked")
+ });
+
+$(".square").click(function(event) {
+  alert("A square was clicked")
+});
